a88d41f155a580b07e3b507da4aaa8ea06d228af
Jesus Rivera <r.jesus9107@gmail.com>
2023-07-29 23:20:05 -0700

    [add] automate wine aggregation

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 import pLimit from 'p-limit'
 import getWineLinksFromPage from './wine/wineLinks.js'
 import getWineData from './wine/wineData.js'
-import { saveWineData, data } from './utils/fileOps.js'
+import { saveWineData } from './utils/fileOps.js'
 
 const page = 1
 const limit = pLimit(5)
-const baseLink = `https://www.bevmo.com/shop/wine/red_wine/other_italian/d/897664#!/?page=${page}` 
+const baseLink = process.argv[2] || `https://www.bevmo.com/shop/wine/white_wine/gewurztraminer/d/897508#!/?page=${page}`
+const type = process.argv[3] || 'multi'
 
 async function main() {
-  // Get all wine links
-  const links = await getWineLinksFromPage(baseLink)
+  // If type is multi, get all wine links 
+  // If type is single, links is null
+  const links = type === 'multi' ? await getWineLinksFromPage(baseLink) : null
 
-  // Log wine link length
-  console.log(`Found ${links.length} wine links`)
+  // If links is null, get wine data from baseLink
+  if (!links) {
+    console.log("here")
+    const wineData = await getWineData(baseLink)
+    saveWineData(wineData)
+  } else {
 
-  // Get wine data
-  const wineData = await Promise.all(links.map(link => limit(() => getWineData(link))))
+    // Log wine link length
+    console.log(`Found ${links.length} wine links`)
+    // Get wine data from each link
+    const wineData = await Promise.all(links.map(link => limit(() => getWineData(link))))
 
-  // Save wine data
-  saveWineData(wineData)
+    // Save wine data
+    saveWineData(wineData)
+  }
 }
 
 main()
--- utils/fileOps.js
+++ utils/fileOps.js
@@ -8,31 +8,36 @@ const isWineValid = wineObj => {
   return wineObj && !data.find(item => item.sku === wineObj.sku)
 }
 
+const isSingle = wine => !Array.isArray(wine)
+
 // Concat new wine data to existing data
-const concat = (data, results) => {
+const concat = (results) => {
+  if (isSingle(results)) return isWineValid(results) && typeof results === 'object' && [...data, results]
   const newWine = results.filter(wine => isWineValid(wine))
   return [...data, ...newWine]
 }
 
 export const saveWineData = results => {
   // Check if wine is exists in data file
-  results = concat(data, results)
+  results = concat(results)
+  const fileURL = new URL('../../winesearch/src/data/wine.json', import.meta.url)
 
   // Save results to file
-  fs.writeFile('./data/all_wine.json', JSON.stringify(results, null, 4), 'utf8', err => {
+  fs.writeFile(fileURL, JSON.stringify(results, null, 4), 'utf8', err => {
     if (err) throw err
     console.log('The file has been saved!')
   })
+
+  // saveFieldData('type')
 }
 
 const saveFieldData = field => {
-  const fieldData = [ ...new Set(data.map(item => item[field])) ]
+  const fieldData = [...new Set(data.map(item => item[field]))]
   const results = JSON.stringify(fieldData, null, 4)
 
-  fs.writeFile('./data/varietals.json', results, 'utf8', err => {
+  fs.writeFile('../data/varietals.json', results, 'utf8', err => {
     if (err) throw err
     console.log('The file has been saved!')
   })
 }
 
-saveFieldData('type')
